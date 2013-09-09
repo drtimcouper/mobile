fb6403e25ef27eeae41e2136f1064fa59875a0ae
drtimcouper <drtimcouper@gmail.com>
2013-09-09 13:00:22 +0100

    blank lines

--- server/myserver.py
+++ server/myserver.py
@@ -1,11 +1,13 @@
 from datetime import datetime as dt
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello %s' % dt.now()
 
+
 if __name__ == '__main__':
     app.run()
-
