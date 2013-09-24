8d2be656e02015685dacef83c6864ff6e2ff6134
Tim Couper <drtimcouper@gmail.com>
2013-09-24 17:15:07 +0100

    added popup

--- ptel/main.py
+++ ptel/main.py
@@ -1,34 +1,39 @@
 import kivy
 kivy.require('1.7.2')
 
+import weakref
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.properties import ListProperty
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.label import Label
-#import pygame.font
-HEIGHT=100
+from kivy.properties import NumericProperty
+from kivy.uix.modalview import ModalView
+from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
+from kivy.core.window import Window
 
-def pix_width(s):
-    "return the representation of s in the current font and size"
-    lbl = Label(text='')
-    fo = pygame.font.SysFont(lbl.font_name, int(lbl.font_size))
-    total = 0
-    for metric in fo.metrics(s):
-        total+=metric[1] + metric[4]
-    #print '%s = %d' % (s,total)
-    return total
-cbs={}
+
+
+HEIGHT=100
+name_width, tel_width = 400,270#253,149
+
 
 class RootWidget(ScrollView):
 
     def __init__(self, **kwargs):
         super(RootWidget, self).__init__(**kwargs)
+        self.build_grid()
+
+
+    def get_names(self):
         names = []
-        #name_width = tel_width = 0
+
         for i in range(40):
             if i ==2:
                 name = 'a very long name so there'
@@ -36,26 +41,30 @@ class RootWidget(ScrollView):
                 name = 'Name %d' % i
             tel = '01582 123 45%d' % i
             names.append((name,tel))
-        #     name_width = max(name_width, pix_width(name))
-        #     tel_width = max(tel_width, pix_width(tel))
-        # print name_width, tel_width
-        name_width, tel_width = 400,270#253,149
-        layout = GridLayout(cols=3,size_hint_y=None,
+
+        return names
+
+    def build_grid(self):
+
+        grid = GridLayout(cols=3,size_hint_y=None,
                                         row_force_default=True, row_default_height=HEIGHT,
                                         )
-        layout.bind(minimum_height=layout.setter('height'))
-        for name, tel in names:
+        grid.bind(minimum_height=grid.setter('height'))
+        for name, tel in self.get_names()[:4]:
             nbtn = NameButton(text=name, size_hint_x=None, width=name_width)
-            layout.add_widget(nbtn)
+            grid.add_widget(nbtn)
             tbtn = TelButton(text=tel, size_hint_x=None, width=tel_width)
-            layout.add_widget(tbtn)
+            grid.add_widget(tbtn)
             cb = CheckBox(size_hint_x=None, width=HEIGHT)  # so it's square
-            layout.add_widget(cb)
-            nbtn.cb = cb
-        self.add_widget(layout)
+            grid.add_widget(cb)
+            nbtn.cb = tbtn.cb = weakref.ref(cb)
+
+        self.add_widget(grid)
+
 
 class NameButton(Button):
     pressed = ListProperty([0, 0])
+
 
     def __init__(self, **kwargs):
         super(NameButton, self).__init__(**kwargs)
@@ -70,9 +79,30 @@ class NameButton(Button):
 
     def on_pressed(self, instance, pos):
         # Show the notes:
-        self.cb.active = not self.cb.active
+        popup=MyPopup(title='Notes: %s' % self.text, anchor_y='top',
+                                    size_hint=(.8,.5))
+        popup.build(self.text)
+        popup.open()
+
+STORAGE={}
+
+class MyPopup(Popup):
+
+    def build(self, name):
+        self.name = name
+        s=STORAGE.get(self.name, '')
+        self.tx = TextInput(text=s)
+        self.content = self.tx
+
+    def on_dismiss(self):
+        STORAGE[self.name] = self.tx.text
+        Window.release_keyboard()
+        super(MyPopup,self).on_dismiss()
+
 
 class TelButton(Button):
+    pressed = ListProperty([0, 0])
+
     def __init__(self, **kwargs):
         super(TelButton, self).__init__(**kwargs)
 
@@ -86,7 +116,8 @@ class TelButton(Button):
 
     def on_pressed(self, instance, pos):
         # dial the number
-        pass
+        cb = self.cb()
+        cb.active = not cb.active
 
 
 
